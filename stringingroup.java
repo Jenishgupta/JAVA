1bb0194dc254cc66e8d40ac2780a43eba187a51b
Jenishgupta <jenishgupta6880@gmail.com>
2025-10-08 13:35:50 +0530

    add name

--- stringingroup.java
+++ stringingroup.java
@@ -1,7 +1,7 @@
 class stringingroup {
 	public static void main(String[] args) 
 	{
-		String [] ia = {"JEN","AKA","JOHN"};
+		String [] ia = {"JEN","AKA","JOHN","AKSH","RAKA"};
 		
 		for (String value: ia) {
 			System.out.println("NAME:-" +value);
