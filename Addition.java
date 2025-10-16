dfb7a7783d667e20b96730ac5eaa98684013757a
Jenishgupta <jenishgupta6880@gmail.com>
2025-10-16 12:39:44 +0530

    update
    Adding comments and statement

--- Addition.java
+++ Addition.java
@@ -1,10 +1,16 @@
+// Java program to add two numbers and display the result
 class Addition 
 {
 	public static void main(String[] args) 
 	{
+		// declaring and initializing two integer variables
 		int a = 10;
 		int b = 20;
+		
+		// performing addition
 		int c = a + b;
-		System.out.println("Result:" + c);
+		
+		// displaying the result
+		System.out.println("Result: " + c);
 	}
 }
--- Addition2.java
+++ Addition2.java
@@ -1,10 +1,16 @@
+// Java program to add two numbers using command-line arguments
 class Addition2
 {
 	public static void main(String[] args) 
 	{
+		// converting command-line arguments from String to integer
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[1]);
+
+		// performing addition
 		int c = a + b;
-		System.out.println("Result:" + c);
+
+		// displaying the result
+		System.out.println("Result: " + c);
 	}
 }
