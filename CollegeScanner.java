0c622d483d083348fdf3e0c80ed9209e657e8e68
Jenishgupta <jenishgupta6880@gmail.com>
2025-10-16 12:31:39 +0530

    one class

---
+++ CollegeScanner.java
@@ -0,0 +1,58 @@
+/* Develop a program to read student values from command line
+store those values in the Student class object, and read
+those values from Student object print on console using Scanner and one class
+Must read 
+   sno, sname, course, fee, email, mobile, 
+   gender and studyingStatus */
+   
+import java.util.Scanner;   
+class  CollegeScanner
+{
+	public static void main(String[] args) 
+	{
+		Scanner scn = new Scanner(System.in);
+		Student st = new Student();
+		
+		System.out.println("=== Enter Student Details ===");
+		System.out.print("Enter eno:- ");
+		st.sno	= scn.nextInt();  
+		scn.nextLine();
+		
+		System.out.print("Enter ename: ");
+		st.sname = scn.nextLine();
+		
+		System.out.print("Enter course name: ");
+        st.course = scn.nextLine();
+
+        System.out.print("Enter course fee: ");
+        st.fee = scn.nextDouble();
+        scn.nextLine();
+
+        System.out.print("Enter email: ");
+        st.email = scn.nextLine();
+
+        System.out.print("Enter mobile number: ");
+        st.mobile = scn.nextLong();
+        scn.nextLine();
+
+        System.out.print("Enter gender (M/F): ");
+        st.gender = scn.next().charAt(0);
+
+        System.out.print("studyingStatus? (true/false): ");
+        st.studyingStatus = scn.nextBoolean();
+		
+		// Display the  printing details
+        System.out.println("\n=== Student Details ===");
+        System.out.println("Student No.     : " + st.sno);
+        System.out.println("Name            : " + st.sname);
+        System.out.println("Course          : " + st.course);
+        System.out.println("Fee             : " + st.fee);
+        System.out.println("Email           : " + st.email);
+        System.out.println("Mobile          : " + st.mobile);
+        System.out.println("Gender          : " + st.gender);
+        System.out.println("Studying Status : " + st.studyingStatus);
+
+        scn.close();
+		
+	}
+}
